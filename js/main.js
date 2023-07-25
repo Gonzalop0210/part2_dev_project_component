c7e51117d5a9c5fec1ee671429ceaa2727f79c31
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-07-25 15:10:01 -0500

    Falta ubicar los elementos en la página

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const text_1_1 = document.querySelector('.text_1_1');
+const text_1_2 = document.querySelector('.text_1_2');
+const text_1_3 = document.querySelector('.text_1_3');
+
+text_1_1.textContent = '<Input />';
+text_1_2.textContent = '&:hover';
+text_1_3.textContent = '&:focus';
+
+const text_2_1 = document.querySelector('.text_2_1');
+const text_2_2 = document.querySelector('.text_2_2');
+const text_2_3 = document.querySelector('.text_2_3');
+
+text_2_1.textContent = '<Input error />';
+text_2_2.textContent = '&:hover';
+text_2_3.textContent = '&:focus';
+
+const text_3_1 = document.querySelector('.text_3_1');
+
+text_3_1.textContent = '<Input disabled />';
+
+const text_4_1 = document.querySelector('.text_4_1');
+const text_4_2 = document.querySelector('.text_4_2');
+
+text_4_1.textContent = '<Input helperText=”Some interesting text” />';
+text_4_2.textContent = '<Input helperText=”Some interesting text” error />';
+
+const text_5_1 = document.querySelector('.text_5_1');
+const text_5_2 = document.querySelector('.text_5_2');
+
+text_5_1.textContent = '<Input startIcon />';
+text_5_2.textContent = '<Input endIcon />';
+
+const text_6_1 = document.querySelector('.text_6_1');
+
+text_6_1.textContent = '<Input value=”text” />';
+
+const text_7_1 = document.querySelector('.text_7_1');
+const text_7_2 = document.querySelector('.text_7_2');
+
+text_7_1.textContent = '<Input size=”sm” />';
+text_7_2.textContent = '<Input size=”md” />';
+
+const text_8_1 = document.querySelector('.text_8_1');
+
+text_8_1.textContent = '<Input fullWidth />';
+
+const text_9_1 = document.querySelector('.text_9_1');
+
+text_9_1.textContent = '<Input multiline row=”4” />';
